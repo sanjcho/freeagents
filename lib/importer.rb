d0cf8f4fedf7907ebb786c0e40ef7128491596a4
sanjcho <sanjcho@gmail.com>
2016-07-07 08:14:46 +0500

    parsing work well, need to change caphit to real from int and some optimisation

--- lib/importer.rb
+++ lib/importer.rb
@@ -9,33 +9,35 @@ class Importer
   end
 
   def parse_teams
-    Team.delete_all
-    Player.delete_all
+    Team.destroy_all
+
     page = Nokogiri::HTML(open("http://nhlnumbers.com/"))
     page.css("a.team").each do |team_parsed|
       team = Team.new
-      parse_players(team_parsed['href'])
+      parse_players(team_parsed['href'], team)
       team.name = team_parsed.css('img').first['alt']
       team.image = team_parsed.css('img').first['src']
       team.save
     end
   end
 
-  def parse_players(src)
+  def parse_players(src, team)
     page = Nokogiri::HTML(open(src))
     #puts page.css("a.active")[0].text #player names
-    page.css('tr'). each do |player|
-      caphit = player.css('td.caphit')[0].text if player.css('td.caphit')[0]
-      #puts caphit
-
-      if caphit.to_i >= 1 && player.css("a.active")[0]
-        puts get_role(player.css('a')[0].to_s) if is_UFA?(player)
+    page.css('tr'). each do |player_page|
+      caphit = player_page.css('td.caphit')[0].text if player_page.css('td.caphit')[0]
+      if caphit.to_i >= 1 && player_page.css("a.active")[0] && is_UFA?(player_page)
+        player = team.players.new
+        puts player.name = player_page.css("a.active")[0].text
+        player.role = get_role(player_page.css('a')[0].to_s)
+        player.caphit = caphit.to_i
+        player.save
       end
     end
   end
 
-  def is_UFA?(player)
-    player.css('td')[5, 3].each do |maybeUFA|
+  def is_UFA?(player_page)
+    player_page.css('td')[5, 3].each do |maybeUFA|
       if maybeUFA && maybeUFA.text.strip =="UFA"
         return true
       end
