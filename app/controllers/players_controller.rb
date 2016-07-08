cb1caff291e0996bdedca38eea2b5f0b73bc7879
Aleksandr Makarenko <sanjcho@gmail.com>
2016-07-08 16:29:21 +0500

    player list rendered, ordered, no pagination...

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,14 +4,14 @@ class PlayersController < ApplicationController
   end
 
   def season1718
-    @players = Player.season1718
+    @players = Player.season1718.order(caphit: :desc)
   end
 
   def season1819
-    @players = Player.season1819
+    @players = Player.season1819.order(caphit: :desc)
   end
 
   def season1920
-    @players = Player.season1920
+    @players = Player.season1920.order(caphit: :desc)
   end
 end
