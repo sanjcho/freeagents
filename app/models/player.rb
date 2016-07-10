6f73c105ac20c2c48379c28c3d9f2e9a95f59d32
sanjcho <sanjcho@gmail.com>
2016-07-10 23:33:30 +0500

    some really small refactoring

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,7 @@ class Player < ActiveRecord::Base
   scope :forward, -> {where(role: "Forward")}
   scope :defenceman, -> {where(role: "Defenceman")}
   scope :goalkeeper, -> {where(role: "Goaltender")}
-  scope :season1718, -> {where(season: "18")}
-  scope :season1819, -> {where(season: "19")}
-  scope :season1920, -> {where(season: "20")}
+  scope :season1718, -> {where(season: "18").order(caphit: :desc)}
+  scope :season1819, -> {where(season: "19").order(caphit: :desc)}
+  scope :season1920, -> {where(season: "20").order(caphit: :desc)}
 end
