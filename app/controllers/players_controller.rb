ae798ded45dc8cc3bdda718530e5f063b67d6368
sanjcho <sanjcho@gmail.com>
2016-07-15 21:27:14 +0500

    controller refactoring

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,32 +3,16 @@ class PlayersController < ApplicationController
   def players_by_seasons
   end
 
-  def season1718
-    @forwards = Player.season1718.forward.page(params[:forward_page])
-    @defencemans = Player.season1718.defenceman.page(params[:defenceman_page])
-    @goalkeepers = Player.season1718.goalkeeper.page(params[:goalkeeper_page])
-    @header = "Season 2017 - 2018"
-    render "season_head"
-  end
-
-  def season1819
-    @forwards = Player.season1819.forward.page(params[:forward_page])
-    @defencemans = Player.season1819.defenceman.page(params[:defenceman_page])
-    @goalkeepers = Player.season1819.goalkeeper.page(params[:goalkeeper_page])
-    @header = "Season 2018 - 2019"
-    render "season_head"
-  end
-
-  def season1920
-    @forwards = Player.season1920.forward.page(params[:forward_page])
-    @defencemans = Player.season1920.defenceman.page(params[:defenceman_page])
-    @goalkeepers = Player.season1920.goalkeeper.page(params[:goalkeeper_page])
-    @header = "Season 2019 - 2020"
-    render "season_head"
+  def season
+    @forwards = Player.where(season: params[:season], role: "Forward").order(caphit: :desc).page(params[:forward_page])
+    @defencemans = Player.where(season: params[:season], role: "Defenceman").order(caphit: :desc).page(params[:defenceman_page])
+    @goalkeepers= Player.where(season: params[:season], role: "Goaltender").order(caphit: :desc).page(params[:goalkeeper_page])
+#    @players = Player.where(season: params[:season]).order(caphit: :desc).page(params[:player_page])
+    @header = "Season 20#{params[:season].to_i-1} - 20#{params[:season]}"
   end
 
   private
     def player_params
-      params.require(:player).permit(:forward_page, :defenceman_page, :goalkeeper_page)
+      params.require(:player).permit(:forward_page, :defenceman_page, :goalkeeper_page, :season)
     end
 end
